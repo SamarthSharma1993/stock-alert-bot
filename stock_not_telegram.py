a5ea1389b222fb485f557de58b40b4e4aae3f21a
SAMARTH SHARMA <78916997+SamarthSharma1993@users.noreply.github.com>
2026-04-26 15:26:12 +0530

    Update stock_not_telegram.py

--- stock_not_telegram.py
+++ stock_not_telegram.py
@@ -1,6 +1,9 @@
 import os
 import requests
 
+print("BOT_TOKEN exists:", "BOT_TOKEN" in os.environ)
+print("CHAT_ID exists:", "CHAT_ID" in os.environ)
+
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
