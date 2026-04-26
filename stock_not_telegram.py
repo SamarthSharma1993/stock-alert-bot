794295c89a447dc3ad811aa91c9fd6f3ad908daa
SAMARTH SHARMA <78916997+SamarthSharma1993@users.noreply.github.com>
2026-04-26 15:15:34 +0530

    Update stock_not_telegram.py modifications

--- stock_not_telegram.py
+++ stock_not_telegram.py
@@ -1,15 +1,12 @@
-import requests
 import os
+import requests
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
 def send_msg(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    response = requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": msg
-    })
-    print(response.json())
+    res = requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    print(res.json())
 
-send_msg("Test message from Python 🚀")
+send_msg("Test from GitHub 🚀")
